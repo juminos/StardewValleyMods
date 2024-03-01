ce3e35b772cb878982899eb3090b332a8333bebc
breakfast-first <princeferozeshah@gmail.com>
2024-03-01 00:39:56 -0500

    gitignore

--- MorePetBreeds/obj/Debug/net6.0/MorePetBreeds.AssemblyInfo.cs
+++ MorePetBreeds/obj/Debug/net6.0/MorePetBreeds.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MorePetBreeds")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21639eb745d7b605ea538249f1278a86f015ee16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5129e4108472d10fd89c68e63322fdacd04aa1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MorePetBreeds")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MorePetBreeds")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
