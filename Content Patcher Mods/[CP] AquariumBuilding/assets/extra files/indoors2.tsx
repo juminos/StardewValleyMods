6c23259c75e098ab2a9f109afe9600ff8daf2d86
Feroze Shah <princeferozeshah@gmail.com>
2024-12-19 23:19:12 -0500

    upadte

--- Content Patcher Mods/[CP] AquariumBuilding/assets/extra files/indoors2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="indoors2" tilewidth="16" tileheight="16" tilecount="128" columns="32">
- <image source="../../../../Content (unpacked)/Maps/townInterior_2" width="512" height="64"/>
-</tileset>
