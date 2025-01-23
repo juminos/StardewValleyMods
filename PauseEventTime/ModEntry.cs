0e66600b6086ef0282de95f8ed8331be0962c9d4
Feroze Shah <princeferozeshah@gmail.com>
2025-01-23 15:56:48 -0500

    more mods

---
+++ PauseEventTime/ModEntry.cs
@@ -0,0 +1,28 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+
+namespace PauseEventTime
+{
+    public class ModEntry: Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            // Subscribe to game loop events
+            Helper.Events.GameLoop.TimeChanged += OnTimeChanged;
+        }
+
+        private void OnTimeChanged(object? sender, TimeChangedEventArgs e)
+        {
+            // Check if an event (cutscene) is active
+            if (Game1.eventUp)
+            {
+                Game1.isTimePaused = true; // Freeze time during events
+            }
+            else
+            {
+                Game1.isTimePaused = false; // Unfreeze time when no event is active
+            }
+        }
+    }
+}
