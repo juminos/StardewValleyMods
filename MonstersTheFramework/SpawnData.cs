b314f88c5260be786c89d86359be651556ee7eb0
Feroze Shah <princeferozeshah@gmail.com>
2025-02-16 02:28:09 -0500

    monsters framework

---
+++ MonstersTheFramework/SpawnData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace MonstersTheFramework
+{
+    public enum SpawnTryTime
+    {
+        OnDayStart,
+        OnLocationChange,
+        OnTimeChange,
+    }
+
+    public class SpawnData
+    {
+        public List<Weighted<string>> Who { get; set; }
+        public SpawnTryTime When { get; set; }
+        public string Where { get; set; }
+        public Rectangle? WhereArea { get; set; }
+        public int HowMany { get; set; } = 1;
+    }
+}
---
+++ MonstersTheFramework/docs/release-notes.md
@@ -0,0 +1,12 @@
+﻿﻿[← back to readme](README.md)
+
+# Release notes
+## 1.0.0
+Released 15 January 2022 for SMAPI 3.13.0 or later.
+
+* Initial release
+
+## 1.0.0-unofficial-1.6
+Released 16 February 2025 for SMAPI 4.1.10 or later.
+
+* Updated for 1.6.15
